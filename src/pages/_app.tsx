039a71f22d9ece712ac92c15f3bf8ece0ac30928
Renan Oliveira <renanlido@yahoo.com.br>
2021-04-18 00:50:49 -0300

    feat: header and button

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import { AppProps } from 'next/app';
 
+import { Header } from '../components/Header';
+
 import '../../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
