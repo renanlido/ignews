fbf911e18a2e74a0a38b2351067b2b0aef7bc5cb
Renan Oliveira <renanlido@yahoo.com.br>
2021-04-22 22:47:50 -0300

    feat: integrate stripe payment method

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,8 @@
+import { useSession, signIn } from 'next-auth/client';
+
+import { api } from '../../services/api';
+import { getStripJs } from '../../services/stripejs.p';
+
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,10 +10,33 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton ({ priceId }: SubscribeButtonProps) {
+  const [ session ] = useSession();
+  
+  async function handleSubscribe() {
+    if(!session) {
+      signIn('github');
+      return;
+    }
+
+    //ANCHOR -> Criar uma checkout session com o Strip.
+    try{
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripJs();
+
+      await stripe.redirectToCheckout({sessionId});
+    }catch (err){
+      alert(err.message);
+    }
+  }
+
   return(
     <button
       type='button'
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
